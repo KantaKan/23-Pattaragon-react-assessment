1f77619fc69af56ebddba74b0b422e46bbe94a76
Ben Taikam <bentaikam@Bens-MacBook-Air.local>
2024-11-06 10:01:02 +0700

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../../practice_react/vite-project/src/Components/Navbar";
+import { useState } from "react";
 
+export default function App () {
+  const [users, setUsers] = useState ([]);
+
+
+
+return (
+  <div className="p-6">
+    <BrowserRouter>
+     <Navbar />
+     <Routes>
+      <Route>
+        
+      </Route>
+     </Routes>
+    </BrowserRouter>
+  </div>
+)
+
+}
