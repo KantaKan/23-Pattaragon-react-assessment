61605f896aeb3921071122bea85954fba660a0b9
Ben Taikam <bentaikam@Bens-MacBook-Air.local>
2024-11-06 10:40:04 +0700

    add pages and create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "../../practice_react/vite-project/src/Components/Navbar";
 import { useState } from "react";
+import Navbar from "../../practice_react/vite-project/src/Components/Navbar";
+import Home from "../../React-Assessment-P/src/pages/Home";
+import HomeAdmin from "./Pages/Home-Admin";
+import HomeUser from "./Pages/Home-User";
+import Owner from "./Pages/Owner";
+
 
 export default function App () {
   const [users, setUsers] = useState ([]);
@@ -12,7 +17,10 @@ return (
     <BrowserRouter>
      <Navbar />
      <Routes>
-      <Route>
+     <Route path="/" element={<Home />} />
+     <Route path="/user" element={<HomeUser />} />
+     <Route path="/admin" element={<HomeAdmin />} />
+     <Route path="/owner" element={<Owner />} > 
         
       </Route>
      </Routes>
---
+++ src/Pages/Owner.jsx
@@ -0,0 +1,9 @@
+export default function Owner() {
+    
+    return (
+        <div>
+         <h1>Owner Page</h1>
+         <p>Welcome to the Owner Page!</p>
+        </div>
+    );
+    }
