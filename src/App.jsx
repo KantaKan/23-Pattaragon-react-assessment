ba0ab66b6a3ab2be6b785a814adf535cb0ef365f
Ben Taikam <bentaikam@Bens-MacBook-Air.local>
2024-11-06 15:46:27 +0700

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
+//เรียกใช้ Bulid in function และนำเข้า components ต่างๆของเว็บ 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import Navbar from "../../practice_react/vite-project/src/Components/Navbar";
-import Home from "../../React-Assessment-P/src/pages/Home";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home-Normal";
 import HomeAdmin from "./Pages/Home-Admin";
 import HomeUser from "./Pages/Home-User";
 import Owner from "./Pages/Owner";
 
-
+// กำหนด useState สำหรับเก็บข้อมูลUsre โดยดึงข้อมูลจาก localStorage 
 export default function App () {
-  const [users, setUsers] = useState ([]);
+  const [users, setUsers] = useState (() => {
+    const storedUsers = localStorage.getItem("users");
+    return storedUsers ? JSON.parse(storedUsers) : [];
+  });
 
+// ใช้ useEffect เพื่อบันทึกข้อมูล users ไปยัง localStorage ทุกครั้งที่ users มีการเปลี่ยนแปลง
+  useEffect(() => {
+    localStorage.setItem("users",JSON.stringify(users))
+  }, [users]);
 
+// Function สำหรับเพิ่ม users
+  const addUser = (newUser) => {
+    setUsers([...users, newUser]);
+  };
 
+// Function สำหรับลบ users
+  const deleteUser = (index) => {
+    setUsers(users.filter((_, i) => i !== index));
+  };
+
+// กำหนดเส้นทางในเว็บด้วย BrowserRouter และ กำหนด Routes สำหรับเส้นทางไปยังหน้าต่าง ๆ
 return (
-  <div className="p-6">
+  <div>
     <BrowserRouter>
      <Navbar />
      <Routes>
      <Route path="/" element={<Home />} />
-     <Route path="/user" element={<HomeUser />} />
-     <Route path="/admin" element={<HomeAdmin />} />
+     <Route path="/user" element={<HomeUser users={users}/>} />
+     <Route path="/admin" element={<HomeAdmin users={users} addUser={addUser} deleteUser={deleteUser} />} />
      <Route path="/owner" element={<Owner />} > 
         
       </Route>
@@ -27,5 +45,4 @@ return (
     </BrowserRouter>
   </div>
 )
-
-}
+};
--- src/Pages/Home-Normal.jsx
+++ src/Pages/Home-Normal.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 export default function Home () {
-    return Home(
+    return (
         <>
             <div className='flex flex-col items-center mt-14'>
             <h2 className='text-[4rem] font-bold'>Generation Thailand</h2>
@@ -9,10 +9,10 @@ export default function Home () {
             </div>
 
             <div className='flex justify-center'>
-            <button className='w-[16rem] rounded-md shadow-2xl bg-white mt-[3rem] px-3 py-3 mr-[10rem] text-[1.5rem]  leading-6 text-black hover:bg-gray-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 '>
+            <button className='w-[16rem] rounded-md  bg-white mt-[3rem] px-3 py-3 mr-[10rem] text-[1.5rem]  leading-6 text-black hover:bg-gray-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 '>
                 <Link to = {'/user'}>User Home Sector</Link>
             </button>
-            <button className='w-[16rem] rounded-md shadow-2xl bg-white mt-[3rem] px-3 py-3 text-[1.5rem]  leading-6 text-black hover:bg-gray-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 '>
+            <button className='w-[16rem] rounded-md  bg-white mt-[3rem] px-3 py-3 text-[1.5rem]  leading-6 text-black hover:bg-gray-600 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 '>
                 <Link to = {'/admin'}>Admin Home Sector</Link>
             </button>
             </div>
